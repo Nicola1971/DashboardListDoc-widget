2474d940f0857982593dffa73d7083afdab5b5a6
Nicola <modxit@modx.it>
2017-12-13 17:25:18 +0100

    updated language

--- assets/plugins/dashboarddoclist/lang/nederlands.php
+++ assets/plugins/dashboarddoclist/lang/nederlands.php
@@ -10,12 +10,13 @@
 //   GNU General Public License (GPL - http://www.gnu.org/copyleft/gpl.html)
 //
 
-$_LDlang['all_status'] = 'Alle status';
+$_LDlang['all_status'] = 'Alle';
 $_LDlang['published'] = 'gepubliceerd';
-$_LDlang['unpublished'] = 'onuitgegeven';
+$_LDlang['unpublished'] = 'gedepubliceerd';
 $_LDlang["deleted"] = 'verwijderde';
 
 $_LDlang["Showing"] = 'Resultaat';
 $_LDlang["of"] = 'van';
-$_LDlang["pages"] = 'Pages';
-$_LDlang["total_rows"] = 'Totaal rijen';
+$_LDlang["pages"] = 'Pagina\'s';
+$_LDlang["total_rows"] = 'Totaal';
+$_LDlang["update"] = 'bijwerken';
